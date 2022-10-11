9df76ba3126eb663f1c397a4649378e8b7eee390
unknown <chickie8x.qn@gmail.com>
2022-10-11 09:49:10 +0700

    remove sub-domain

--- src/router/index.js
+++ src/router/index.js
@@ -4,29 +4,14 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
-      name: "home",
-      component: () => import("../views/HomeView/index.vue"),
-    },
-    {
       path: "/login",
       name: "login",
       component: () => import("../views/LoginView/index.vue"),
     },
     {
-      path: "/dashboard",
+      path: "/",
       name: "dashboard",
       component: () => import("../views/DashboardView/index.vue"),
-    },
-    {
-      path: "/dashboard/water",
-      name: "water",
-      component: () => import("../views/WaterView/index.vue"),
-    },
-    {
-      path: "/dashboard/electric",
-      name: "electric",
-      component: () => import("../views/ElectricView/index.vue"),
     },
   ],
 });
